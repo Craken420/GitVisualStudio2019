e1da13714bf4a46f939294f3604cdc68ac8b9f44
Luis Angel Peña <la.pena@mavi.mx>
2021-07-22 11:13:23 -0500

    First Commit

--- GitVisualStudio2019/Form1.cs
+++ GitVisualStudio2019/Form1.cs
@@ -16,5 +16,10 @@ namespace GitVisualStudio2019
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jola");
+        }
     }
 }
